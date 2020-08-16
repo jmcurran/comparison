b0ea2095e71ccf0359a44ffd1647ab5ea6cf6249
James Curran <j.curran@auckland.ac.nz>
2020-08-17 11:48:23 +1200

    added some more tests

--- tests/testCalcLR.R
+++ tests/testCalcLR.R
@@ -29,3 +29,35 @@ stopifnot(all.equal(lr.1, 51.1424311))
 lr.2 = two.level.normal.LR(control, recovered.2, Z)
 
 stopifnot(all.equal(lr.2, 0.0289990757))
+
+
+# test the new calcLR routine - should be identical
+lr.3 = calcLR(control, recovered.1, Z, "mvn")
+stopifnot(all.equal(lr.3, 51.1424311))
+
+lr.4 = calcLR(control, recovered.2, Z, "mvn")
+stopifnot(all.equal(lr.4, 0.0289990757))
+
+
+# calculate the likelihood ratio for a known
+# same source comparison 
+# 2020-08-01 Both this version and the previous version return 20.5896672
+lr.5 = two.level.density.LR(control, recovered.1, Z)
+
+stopifnot(all.equal(lr.5, 20.5896672))
+
+# calculate the likelihood ratio for a known
+# different source comparison
+# 2020-08-01 Both this version and the previous version return 0.0116139204
+lr.6 = two.level.density.LR(control, recovered.2, Z)
+
+stopifnot(all.equal(lr.6, 0.0116139204))
+
+# Test new calcLR - should be identical
+lr.7 = calcLR(control, recovered.1, Z, "kde")
+
+stopifnot(all.equal(lr.7, 20.5896672))
+
+lr.8 = calcLR(control, recovered.2, Z, "kde")
+
+stopifnot(all.equal(lr.8, 0.0116139204))
